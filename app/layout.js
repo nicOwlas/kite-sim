b9ba064f5b910ce6b480a8fbfc6ba725cbf640ba
Nicolas Draber <nicolas.draber@gmail.com>
2023-09-06 12:43:28 +0200

    Update metadata tags

--- app/layout.js
+++ app/layout.js
@@ -1,44 +1,35 @@
+//layout.js
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Kite Sim",
-  description: "Traction Kite Flight Simulator",
+  description:
+    "Simulate kite-powered boat journeys with this Traction Kite Flight Simulator",
+  image:
+    "https://github.com/nicOwlas/kite-sim/blob/main/public/preview.jpg?raw=true",
+  url: "https://kite-sim-nicolasdraber.vercel.app/",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         {/* Open Graph */}
-        <meta property="og:title" content="Kite Simulator" />
-        <meta
-          property="og:description"
-          content="Kite Simulator For Commercial Boat Traction"
-        />
-        <meta
-          property="og:image"
-          content="https://github.com/nicOwlas/kite-sim/blob/main/public/preview.jpg?raw=true"
-        />
-        <meta
-          property="og:url"
-          content="https://kite-sim-nicolasdraber.vercel.app/"
-        />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:url" content={metadata.url} />
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Kite Simulator" />
-        <meta
-          name="twitter:description"
-          content="Kite Simulator For Commercial Boat Traction"
-        />
-        <meta
-          name="twitter:image"
-          content="https://github.com/nicOwlas/kite-sim/blob/main/public/preview.jpg?raw=true"
-        />
-      </head>
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.image} />
+      </Head>
       <body className={inter.className}>
         {children} <Analytics />
       </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,37 +1,8 @@
-import Head from "next/head";
 import App from "./app";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        {/* Open Graph */}
-        <meta property="og:title" content="Kite Simulator" />
-        <meta
-          property="og:description"
-          content="Kite Simulator For Commercial Boat Traction"
-        />
-        <meta
-          property="og:image"
-          content="https://github.com/nicOwlas/kite-sim/blob/main/public/preview.jpg?raw=true"
-        />
-        <meta
-          property="og:url"
-          content="https://kite-sim-nicolasdraber.vercel.app/"
-        />
-
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Kite Simulator" />
-        <meta
-          name="twitter:description"
-          content="Kite Simulator For Commercial Boat Traction"
-        />
-        <meta
-          name="twitter:image"
-          content="https://github.com/nicOwlas/kite-sim/blob/main/public/preview.jpg?raw=true"
-        />
-      </Head>
       <App />
     </>
   );
